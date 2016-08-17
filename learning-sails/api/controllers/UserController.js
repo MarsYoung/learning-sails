4f67050972f3ae948266bc300ceb976a2dfdda41
mazhiyu <444177096@qq.com>
2016-08-17 18:14:46 +0800

    。

---
+++ learning-sails/api/controllers/UserController.js
@@ -0,0 +1,51 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.add()`
+   */
+  add: function (req, res) {
+    return res.json({
+      todo: 'add() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.delete()`
+   */
+  delete: function (req, res) {
+    return res.json({
+      todo: 'delete() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.find()`
+   */
+  find: function (req, res) {
+    return res.json({
+      todo: 'find() is not implemented yet!'
+    });
+  }
+};
+
